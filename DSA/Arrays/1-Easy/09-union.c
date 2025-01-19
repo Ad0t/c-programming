ce4df0768ae54408e2b6edf0ac2bb35fb13ebac2
Ad0t <171239099+Ad0t@users.noreply.github.com>
2025-01-19 15:51:48 +0530

    Arrays

---
+++ DSA/Arrays/1-Easy/09-union.c
@@ -0,0 +1,60 @@
+#include<stdio.h>
+
+void findUnion(int arr1[], int arr2[], int n, int m, int unionArr[], int *unionSize);
+void printArr (int arr[], int n);
+
+void printArr (int arr[], int n) {
+    printf("Array:\t");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
+
+void findUnion(int arr1[], int arr2[], int n, int m, int unionArr[], int *unionSize) {
+    int i = 0, j = 0, k = 0;    // pointers [(i & j) for (arr1 & arr2) and k for unionArr]
+
+    while (i < n && j < m) {
+        if (arr1[i] <= arr2[j]) {
+            if (k == 0 || unionArr[k-1] != arr1[i]) {
+                unionArr[k++] = arr1[i];
+            }
+            i++;
+        } else {
+            if (k == 0 || unionArr[k-1] != arr2[j]) {
+                unionArr[k++] = arr2[j];
+            }
+            j++;
+        }
+    }
+    while (i < n) {
+        if (k == 0 || unionArr[k-1] != arr1[i]) {
+                unionArr[k++] = arr1[i];
+        }
+        i++;
+    }
+    while (j < m) {
+        if (k == 0 || unionArr[k-1] != arr2[j]) {
+                unionArr[k++] = arr2[j];
+        }
+        j++;
+    }
+    *unionSize = k;
+}
+
+int main () {
+    int n = 10, m = 7;
+    int arr1[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    int arr2[] = {2, 3, 4, 4, 5, 11, 12};
+
+    int unionArr[n + m];
+    int unionSize = 0;
+
+
+    findUnion(arr1, arr2, n, m, unionArr, &unionSize);
+    printArr(arr1, n);
+    printArr(arr2, m);
+
+    printArr(unionArr, unionSize);
+    return 0;
+}
---
+++ DSA/Arrays/1-Easy/10-intersection.c
@@ -0,0 +1,55 @@
+#include<stdio.h>
+
+void findIntersection(int arr1[], int arr2[], int n, int m, int unionArr[], int *unionSize);
+void printArr (int arr[], int n);
+
+void printArr (int arr[], int n) {
+    printf("Array:\t");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
+
+void findIntersection(int arr1[], int arr2[], int n, int m, int intersectionArr[], int *intersectionSize) {
+    int i = 0, j = 0, k = 0;    // pointers [(i & j) for (arr1 & arr2) and k for unionArr]
+
+    while (i < n && j < m) {
+        if (arr1[i] < arr2[j]) {
+            // if (k == 0 || intersectionArr[k-1] != arr1[i]) {
+            //     intersectionArr[k++] = arr1[i];
+            // }
+            i++;
+        } else if (arr1[i] > arr2[j]){
+            // if (k == 0 || intersectionArr[k-1] != arr2[j]) {
+            //     intersectionArr[k++] = arr2[j];
+            // }
+            j++;
+        } else {
+            // if (k == 0 || intersectionArr[k-1] != arr1[i]) {
+            //     intersectionArr[k++] = arr1[i];
+            // }
+            intersectionArr[k++] = arr1[i];
+            i++;
+            j++;
+        }
+    }
+    *intersectionSize = k;
+}
+
+int main () {
+    int n = 10, m = 7;
+    int arr1[] = {1, 2, 3, 4, 4, 5, 6, 7, 8, 9, 10};
+    int arr2[] = {2, 3, 4, 4, 5, 11, 12};
+
+    int intersectionArr[n + m];
+    int intersectionSize = 0;
+
+
+    findIntersection(arr1, arr2, n, m, intersectionArr, &intersectionSize);
+    printArr(arr1, n);
+    printArr(arr2, m);
+
+    printArr(intersectionArr, intersectionSize);
+    return 0;
+}
