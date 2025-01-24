178f26a80ecd088ff7edf1e5e4acfbabd1e5447a
Ad0t <171239099+Ad0t@users.noreply.github.com>
2025-01-25 00:09:07 +0530

    Practice

--- CodeWithHarry/05-functions-recursion/practice.c
+++ CodeWithHarry/05-functions-recursion/practice.c
@@ -2,30 +2,112 @@
 /*
 Write a program using function to find average of three numbers.
 */
+void avgOfThree ();
 /*
 Write a function to convert Celsius temperature into Fahrenheit.
 */
+void celsiusToFahrenheit ();
 /*
 Write a function to calculate force of attraction on a body of mass ‘m’ exerted by earth. Consider g = 9.8m/s2.
 */
+void forceOfAttraction ();
 /*
 Write a program using recursion to calculate nth element of Fibonacci series.
 */
+void fibonacciSeries (int n, int arr[]);
 /*
 What will the following line produce in a C program:
 int a = 4;
 printf("%d %d %d \n", a, ++a, a++);
+
+ANSWER: 4 5 4
 */
 /*
 6. Write a recursive function to calculate the sum of first ‘n’ natural numbers.
 */
+int sumOfNaturalNumbers(int n);
 /*
 7. Write a program using function to print the following pattern (first n lines)
 *
 * * *
 * * * * *
 */
+void printPattern(int n);
+
+
+void avgOfThree () {
+    float num1, num2, num3, avg;
+    printf("Enter first number: ");
+    scanf("%f", &num1);
+    printf("Enter second number: ");
+    scanf("%f", &num2);
+    printf("Enter third number: ");
+    scanf("%f", &num3);
+    avg = (num1 + num2 + num3) / 3;
+    printf("Average of three numbers is: %.2f\n", avg);
+}
+
+
+void celsiusToFahrenheit () {
+    float celsius, fahrenheit;
+    printf("Enter temperature in Celsius: ");
+    scanf("%f", &celsius);
+    fahrenheit = (celsius * 9/5) + 32;
+    printf("Temperature in Fahrenheit is: %.2f\n", fahrenheit);
+}
+
+void forceOfAttraction () {
+    float mass, force;
+    printf("Enter mass of the body: ");
+    scanf("%f", &mass);
+    force = mass * 9.8;
+    printf("Force of attraction is: %.2f\n", force);
+}
+
+void fibonacciSeries (int n, int arr[]) {
+    if (n <= 0) {
+        return;
+    } else if (n == 1) {
+        arr[0] = 0;
+        return;
+    } else if (n == 2) {
+        arr[0] = 0;
+        arr[1] = 1;
+        return;
+    } else {
+        fibonacciSeries(n-1, arr);
+        arr[n-1] = arr[n-2] + arr[n-3];
+    }
+}
+int sumOfNaturalNumbers (int n) {
+    if (n == 0) {
+        return 0;
+    }
+    return n + sumOfNaturalNumbers(n-1);
+}
+void printPattern(int n) {
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= i; j++) {
+            printf("*  ");
+        }
+        printf("\n");
+    }
+}
 
 int main () {
 
+
+    // int n = 20;
+    // int arr[n];
+    // fibonacciSeries(n, arr);
+    // printf("Fibonacci Series up to %d terms:\n", n);
+    // for (int i = 0; i < n; i++) {
+    //     printf("%d ", arr[i]);
+    // }
+    // printf("\n");
+
+    // int n = 5, sum;
+    // sum = sumOfNaturalNumbers(n);
+    // printf("Sum of first %d natural numbers is: %d\n", n, sum);
+    printPattern(10);
 }
--- CodeWithHarry/06-pointers/practice.c
+++ CodeWithHarry/06-pointers/practice.c
@@ -4,13 +4,16 @@
 Write a program to print the address of a variable. Use this address to get the
 value of the variable.
 */
+void queso1 ();
 /*
 Write a program having a variable ‘i’. Print the address of ‘i’. Pass this variable to a function and print its address. Are these addresses same? Why?
 */
+int queso2 (int *ptr);
 /*
 Write a program to change the value of a variable to ten times of its current
 value.
 */
+void queso3 (int *a);
 /*
 Write a function and pass the value by reference.
 */
@@ -18,12 +21,60 @@ Write a function and pass the value by reference.
 Write a program using a function which calculates the sum and average of two
 numbers. Use pointers and print the values of sum and average in main().
 */
+int *sum (int a, int b);
+float *avg (int a, int b);
 /*
 Write a program to print the value of a variable i by using “pointer to pointer” type of variable.
 */
+void queso5 ();
 /*
 Try problem 3 using call by value and verify that it does not change the value of the said variable.
 */
-int main () {
+void queso1 () {
+    int x = 10;
+    int *ptr = &x;
+    printf("Address of %d as pointer: %p\n", x, &x);
+    printf("Value at %p is %d\n", ptr, *ptr);
+    printf("Address of %d as unsigned digit: %u\n", x, &x);
+    printf("Value at %u is %d\n", ptr, *ptr);
+    queso2(ptr);
+}
+int queso2 (int *ptr) {
+    printf("OTHER FUNCTION:\nValue at %u is %d\n", ptr, *ptr);
+    return 0;
+}
+void queso3 (int *a) {
+    *a = *a * 10;
+}
+int *sum (int a, int b) {
+    int sum = a + b;
+    int *ptr = &sum;
+    printf("The sum is %d\n", sum);
+    return ptr;
+}
+float *avg (int a, int b) {
+    float avg = (a + b)/2.0;
+    float *ptr = &avg;
+    printf("The avg is %.2f\n", avg);
+    return ptr;
+}
+void queso5 () {
+    int x = 10;
+    int *ptr1 = &x;
+    int **ptr2 = &ptr1;
+    printf("%u -> %d\n", ptr1, *ptr1);
+    printf("%u -> %u -> %d\n", ptr2, *ptr2, **ptr2);
+}
 
+int main () {
+    // queso1 ();
+    // int i = 10;
+    // printf("The value of i: %d\n", i);
+    // queso3 (&i);
+    // printf("The value of i after function call: %d\n", i);
+    // int *sum_ptr = sum(10, 20);
+    // printf("The value of sum_ptr %u: %d\n",sum_ptr, *sum_ptr);
+    // float *avg_ptr = avg(10, 20);
+    // printf("The value of avg_ptr %u: %.2f\n", avg_ptr, *avg_ptr);
+    queso5();
 }
