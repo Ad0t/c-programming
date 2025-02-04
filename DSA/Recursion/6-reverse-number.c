47239c563d3f5efcea24fb5f934a8ac2a540437c
Ad0t <171239099+Ad0t@users.noreply.github.com>
2025-02-04 16:47:35 +0530

    Recursion

---
+++ DSA/Recursion/6-reverse-number.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<math.h>
+
+int reversingNumber (int n, int len) {
+    if (n < 10) {
+        return n;
+    }
+    return ((n%10)*pow(10, len-1) + reversingNumber(n/10, len - 1));
+}
+
+int main () {
+    int x = reversingNumber(1234567, 7);
+    printf("%d", x);
+}
